a76cc7ebdff76f7d471bccac351c632eaf5f73a5
dkoka17 <dkoka17@freeuni.edu.ge>
2020-12-25 08:05:28 -0800

    Initial Commit

---
+++ DialPad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DialPad
+//
+//  Created by dato on 12/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
